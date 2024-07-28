a7f65b336385c271ee26f72eabaaf94c3e026ec5
Haris Rao <harisrao966@gmail.com>
2024-07-28 21:12:27 +0500

    changess

---
+++ src/helper/helper.js
@@ -0,0 +1,43 @@
+export const apiHeader = (token, isFormData) => {
+  if (token && !isFormData) {
+    return {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    };
+  }
+  if (token && isFormData) {
+    return {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "multipart/form-data",
+      },
+    };
+  }
+  if (!token && !isFormData) {
+    return {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+  }
+
+  if (!token && isFormData) {
+    return {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    };
+  }
+};
+
+export const CreateFormData = (data) => {
+  const formData = new FormData();
+  for (let key in data) {
+    formData.append(key, data[key]);
+  }
+  return formData;
+};
+
+export const googleMapApiKey = "";
